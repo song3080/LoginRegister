db07b272ca96afc28566a90be95744419d701f01
song3080 <3080518@gmail.com>
2023-05-10 10:20:49 +0900

    loginregister

--- app/src/main/java/com/example/iothomelogin/LoginActivity.java
+++ app/src/main/java/com/example/iothomelogin/LoginActivity.java
@@ -33,7 +33,7 @@ public class LoginActivity extends AppCompatActivity {
         btn_register.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent( LoginActivity.this, RegisterActivity.class );
+                Intent intent = new Intent( LoginActivity.this, RegisterRequest.RegisterActivity.class );
                 startActivity( intent );
             }
         });
